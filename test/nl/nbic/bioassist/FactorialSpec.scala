7f739f4e085308be4245095642a85653eac54f42
Freek de Bruijn <freekdb@gmail.com>
2013-06-23 23:13:22 +0200

    Small textual change.

--- test/nl/nbic/bioassist/FactorialSpec.scala
+++ test/nl/nbic/bioassist/FactorialSpec.scala
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith
 
 @RunWith(classOf[JUnitRunner])
 class FactorialSpec extends FlatSpec with ShouldMatchers {
-  "A Factorial object" should "return the right value for 42!" in {
+  "The Factorial object" should "return the right value for 42!" in {
     Factorial.factorial(42) should equal (BigInt("1405006117752879898543142606244511569936384000000000"))
   }
 }
